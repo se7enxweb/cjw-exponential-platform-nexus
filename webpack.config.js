a6fd980c90ef84f0f19f7fded97087d71666d193
7x <info@se7enx.com>
2026-02-07 15:47:57 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const Encore = require('@symfony/webpack-encore');
+const path = require('path');
+const getEzConfig = require('./ez.webpack.config.js');
+const eZConfigManager = require('./ez.webpack.config.manager.js');
+
+Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+
+const eZConfig = getEzConfig(Encore);
+const customConfigs = require('./ez.webpack.custom.configs.js');
+
+Encore.reset();
+Encore
+    .setOutputPath('web/assets/build')
+    .setPublicPath('/assets/build')
+    .enableSassLoader()
+    .enableReactPreset()
+    .enableSingleRuntimeChunk();
+
+// Put your config here.
+
+
+
+// uncomment the two lines below, if you added a new entry (by Encore.addEntry() or Encore.addStyleEntry() method) to your own Encore configuration for your project
+// const projectConfig = Encore.getWebpackConfig();
+// module.exports = [ eZConfig, ...customConfigs, projectConfig ];
+
+// comment-out this line if you've uncommented the above lines
+module.exports = [ eZConfig, ...customConfigs ];
